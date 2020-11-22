8abbb2aa14eb9d250bc8ffa063369cd5fb010abb
tetsu-upstr <bodomu@gmail.com>
2020-11-22 15:25:20 +0900

    add rotate

--- ProtoType 2/Assets/Scripts/PlayerController.cs
+++ ProtoType 2/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
     public float turnSpeed;
+    public float horizontalInput;
+    public float forwadInput;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwadInput = Input.GetAxis("Vertical");
+
         // transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwadInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
