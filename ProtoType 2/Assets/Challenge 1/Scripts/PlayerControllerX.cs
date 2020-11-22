f2eed9266336fccce17efd5b36fa779c18aa940a
tetsu-upstr <bodomu@gmail.com>
2020-11-22 19:44:57 +0900

    add plane scene

---
+++ ProtoType 2/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 20;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        //transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
---
+++ ProtoType 2/Assets/Challenge 1/Scripts/propellerSpin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class propellerSpin : MonoBehaviour
+{
+    private float speed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+}
