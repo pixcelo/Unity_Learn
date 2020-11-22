db098933d57b9284a5328e9ee925f1b21d170c58
tetsu-upstr <bodomu@gmail.com>
2020-11-22 14:01:37 +0900

    add camera script

--- ProtoType 2/Assets/Scripts/PlayerController.cs
+++ ProtoType 2/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20;
+    public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +14,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        // transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
