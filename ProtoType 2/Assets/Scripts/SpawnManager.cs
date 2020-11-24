0aa5b02325f6c5d06ae595203f385c91430370e9
tetsu-upstr <bodomu@gmail.com>
2020-11-24 12:32:09 +0900

    add InvokeRepeating

--- ProtoType 2/Assets/Scripts/SpawnManager.cs
+++ ProtoType 2/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnRangeZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -17,7 +19,7 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            SpawnRandomAnimal();
+            
         }
     }
 
