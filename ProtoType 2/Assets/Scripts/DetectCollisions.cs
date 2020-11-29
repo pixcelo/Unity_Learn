aaff3cd5b8341b9110c9994534ab864c80c2a06a
tetsu-upstr <bodomu@gmail.com>
2020-11-29 11:17:59 +0900

    add project

---
+++ ProtoType 2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 衝突判定を行って、ゲームオブジェクトを破壊する
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
