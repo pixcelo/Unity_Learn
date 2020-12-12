2a6af9513efc565ac7afeca3976b04088be91b10
tetsu-upstr <bodomu@gmail.com>
2020-12-13 08:01:16 +0900

    mod OnCollisionEnter

--- ProtoType 4/Assets/Scripts/PlayerController.cs
+++ ProtoType 4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +21,22 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+        }
     }
 }
