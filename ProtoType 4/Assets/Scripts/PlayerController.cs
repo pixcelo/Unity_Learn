a350f64edb3dda221b400da8a1fb993ba3ca5372
tetsu-upstr <bodomu@gmail.com>
2020-12-13 10:05:01 +0900

    add powerup forcer

--- ProtoType 4/Assets/Scripts/PlayerController.cs
+++ ProtoType 4/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
     public float speed = 5.0f;
     public bool hasPowerup;
 
@@ -23,6 +24,7 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 
+    // パワーアップアイテムを取った場合
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Powerup"))
@@ -32,11 +34,18 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // 敵とぶつかった場合
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
             Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+
+            // パワーアップした力で衝撃を加える
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
     }
 }
