3957e902d9774ae7d00c7ad118215828e903a78a
tetsu-upstr <bodomu@gmail.com>
2020-12-15 06:13:37 +0900

    enemy increment

--- ProtoType 4/Assets/Scripts/SpawnManager.cs
+++ ProtoType 4/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
@@ -32,7 +33,8 @@ public class SpawnManager : MonoBehaviour
 
         if (enemyCount == 0)
         {
-            SpawnEnemyWave(1);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
         }
     }
 
