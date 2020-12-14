fd18d61bc0d87f15effe3d8dc49a73a3bab8a7aa
tetsu-upstr <bodomu@gmail.com>
2020-12-15 05:58:50 +0900

    add SpawnEnemyWave

--- ProtoType 4/Assets/Scripts/SpawnManager.cs
+++ ProtoType 4/Assets/Scripts/SpawnManager.cs
@@ -11,7 +11,16 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(3);
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+
     }
 
     // Update is called once per frame
